b62bcc4c3e9da86de2d347feab69c78c9db55b51
LitterAdamDev <adamlitter99@gmail.com>
2021-09-17 15:06:22 +0200

    header separated and makefile added

---
+++ Makefile
@@ -0,0 +1,11 @@
+CC = g++
+CFLAGS = -Wall -g
+
+main: main.o SingletonTable.o 
+	$(CC) $(CFLAGS) -o main main.o SingletonTable.o
+ 
+ 
+main.o: main.cpp SingletonTable.h 
+	$(CC) $(CFLAGS) -c main.cpp
+ 
+SingletonTable.o: SingletonTable.h
---
+++ SingletonTable.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+
+typedef std::vector<std::vector<std::string> > Table;
+
+class SingletonTable
+{
+
+protected:
+    SingletonTable();
+    static SingletonTable* SingletonTable_;
+
+    std::string value_;
+    Table table_;
+    std::string error_;
+    bool isOn;
+    void Edit(const std::string&);
+    void Add(const std::string&);
+    void Delete(const std::string&);
+    void Insert(const std::string&);
+    void Exit();
+    void SetError(const std::string&);
+    void SplitString(const std::string&, std::vector<std::string>&);
+    void SetIsOn(bool);
+
+public:
+    SingletonTable(SingletonTable&) = delete;
+    void operator=(const SingletonTable&) = delete;
+    static SingletonTable *GetInstance();
+    void ExecuteCommand(const std::string&);
+    void PrintTable();
+    std::string PrintError();
+    bool GetIsOn() const;
+};
