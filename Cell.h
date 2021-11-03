248e7f9b96998bcf11d474b991f86cd46ad303df
LitterAdamDev <adamlitter99@gmail.com>
2021-11-03 10:38:13 +0100

    separation of cell class

---
+++ Cell.h
@@ -0,0 +1,72 @@
+#include <vector>
+#include <iostream>
+
+/*! \brief Cell class:
+ * for the table cells
+ */
+class Cell{
+private:
+    enum FormulaType {
+        SUM = 0,
+        AVG,
+        MIN,
+        MAX
+    };
+    int align_ = std::ios::left;
+    std::string value_ = "";
+    bool is_formula_ = false;
+    bool is_formula_right = false;
+    unsigned TLR;
+    unsigned TLC;
+    unsigned BRR;
+    unsigned BRC;
+    bool evaluated;
+    FormulaType type_;
+    std::string formula_ = "";
+    void CalculateFormula(const std::vector<std::vector<Cell>>&, const std::string&);
+public:
+    Cell(const std::vector<std::vector<Cell>>&){}        /*! constructor of Cell class */
+    Cell(const std::vector<std::vector<Cell>>& table, std::string value, int align = std::ios::left):align_{align}{
+        CalculateFormula(table,value);
+    }   
+    static void Refresh(const std::vector<std::vector<Cell>>& table,const std::string&);
+    /*!  = operator overloading */
+    Cell& operator=(Cell other){
+        align_=other.align_;
+        value_=other.value_;
+        is_formula_ = other.is_formula_;
+        is_formula_right = other.is_formula_right;
+        TLR = other.TLR;
+        TLC = other.TLC;
+        BRR = other.BRR;
+        BRC = other.BRC;
+        evaluated = true;
+        type_ = other.type_;
+        formula_ = other.formula_;
+        return *this;
+    }
+    int GetAlign() const{ /*! return value of align */
+        return align_;
+    }
+    std::string GetValue() const{   /*! return the value or the formula of the cell */
+        if(this->is_formula_ && this->is_formula_right){
+            return formula_;
+        }else{
+            return value_;
+        }
+    }
+    std::string GetPrint() const{  /*! get the value of print */
+        return value_;
+    }
+    std::string GetFormula() const{  /*! get the formula */
+        return formula_;
+    }
+    void SetAlign(int align){       /*! set the value of align */
+        this->align_ = align;
+    }
+    void SetValue(const std::vector<std::vector<Cell>>& table, std::string value){  /*! set the value of the cell with CalculateFormula */
+        CalculateFormula(table,value);
+    }
+    friend bool operator<(const Cell& a,const Cell& b);
+    friend bool operator>(const Cell& a,const Cell& b);
+};
--- SingletonTable.h
+++ SingletonTable.h
@@ -8,78 +8,7 @@
 #include <fstream>
 #include <sstream>
 #include <float.h>
-
-/*! \brief Cell class:
- * for the table cells
- */
-class Cell{
-private:
-    enum FormulaType {
-        SUM = 0,
-        AVG,
-        MIN,
-        MAX
-    };
-    int align_ = std::ios::left;
-    std::string value_ = "";
-    bool is_formula_ = false;
-    bool is_formula_right = false;
-    unsigned TLR;
-    unsigned TLC;
-    unsigned BRR;
-    unsigned BRC;
-    bool evaluated;
-    FormulaType type_;
-    std::string formula_ = "";
-    void CalculateFormula(const std::vector<std::vector<Cell>>&, const std::string&);
-public:
-    Cell(const std::vector<std::vector<Cell>>&){}        /*! constructor of Cell class */
-    Cell(const std::vector<std::vector<Cell>>& table, std::string value, int align = std::ios::left):align_{align}{
-        CalculateFormula(table,value);
-    }   
-    static void Refresh(const std::vector<std::vector<Cell>>& table,const std::string&);
-    /*!  = operator overloading */
-    Cell& operator=(Cell other){
-        align_=other.align_;
-        value_=other.value_;
-        is_formula_ = other.is_formula_;
-        is_formula_right = other.is_formula_right;
-        TLR = other.TLR;
-        TLC = other.TLC;
-        BRR = other.BRR;
-        BRC = other.BRC;
-        evaluated = true;
-        type_ = other.type_;
-        formula_ = other.formula_;
-        return *this;
-    }
-    int GetAlign() const{ /*! return value of align */
-        return align_;
-    }
-    std::string GetValue() const{   /*! return the value or the formula of the cell */
-        if(this->is_formula_ && this->is_formula_right){
-            return formula_;
-        }else{
-            return value_;
-        }
-    }
-    std::string GetPrint() const{  /*! get the value of print */
-        return value_;
-    }
-    std::string GetFormula() const{  /*! get the formula */
-        return formula_;
-    }
-    void SetAlign(int align){       /*! set the value of align */
-        this->align_ = align;
-    }
-    void SetValue(const std::vector<std::vector<Cell>>& table, std::string value){  /*! set the value of the cell with CalculateFormula */
-        CalculateFormula(table,value);
-    }
-    friend bool operator<(const Cell& a,const Cell& b);
-    friend bool operator>(const Cell& a,const Cell& b);
-};
-
-
+#include <Cell.h>
 
 typedef std::vector<std::vector<Cell>> Table; /*! definition of Table vector */
 
@@ -93,7 +22,6 @@ protected:
     SingletonTable(std::string); 
     SingletonTable(std::string,std::string);
     static SingletonTable* SingletonTable_;
-
     std::string value_;
     Table table_;
     std::string error_;
@@ -125,8 +53,6 @@ protected:
     std::string& string_toupper(std::string& myst);                     
     std::string& string_toupper(std::string&& myst);
 
-
-    
 public:
     SingletonTable(SingletonTable&) = delete;                           
     void operator=(const SingletonTable&) = delete;
